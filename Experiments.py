9360304ef1b8a742dc1ea9c4d44d0d086be42d59
u-adrian <uuesb@student.kit.edu>
2022-01-03 17:18:00 +0100

    Name changes: TEST.py -> Experiments.py, MainClass.py -> Trainer.py, added an Evaluator.py file

---
+++ Experiments.py
@@ -0,0 +1,12 @@
+import Trainer
+
+
+def main():
+    Trainer.train(device="GPU", generator="small_gan", discriminator="small_gan", criterion="BCELoss",
+                    learning_rate="0.0001", real_img_fake_label="True", num_epochs="200", noise_size="10")
+    # Trainer.train(device="GPU", generator="resNet", discriminator="resNet", criterion="BCELoss",
+    #                 learning_rate="0.0001", real_img_fake_label="True", num_epochs="5", noise_size="100")
+
+
+if __name__ == "__main__":
+    main()
