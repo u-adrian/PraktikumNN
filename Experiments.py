62cf58a3e53b4e0028224f7d970638915c9181a1
u-adrian <uuesb@student.kit.edu>
2022-01-08 18:32:36 +0100

    Tested code using Experiments, cleanup still needed

--- Experiments.py
+++ Experiments.py
@@ -4,39 +4,36 @@ from scores import inception_score_cifar10
 
 
 def main():
-
     '''
-
-    unique_name = Trainer.train_and_get_uniquename(device="GPU", generator="small_gan", discriminator="small_gan",
-                                                   criterion="BCELoss", learning_rate="0.0001",
-                                                   real_img_fake_label="True", num_epochs="5", noise_size="20",
-                                                   snapshot_interval="1", name="smallGan01")
+    unique_name = Trainer.train(device="GPU", generator="small_gan", discriminator="small_gan", criterion="BCELoss",
+                                learning_rate="0.0001", real_img_fake_label="True", num_epochs="5", noise_size="20",
+                                snapshot_interval="1", name="smallGan01")
+    model_path = f'./models/{unique_name}/gan_latest'
+    output_path = f'./data/{unique_name}/results'
+    Evaluator.create_images(device="GPU", generator="small_gan", noise_size="20", model_path=model_path,
+                            output_path=output_path)
 
     unique_name = Trainer.train(device="GPU", generator="res_net_depth1", discriminator="res_net_depth1",
-                                criterion="BCELoss", learning_rate="0.0001",
-                                real_img_fake_label="True", num_epochs="5", noise_size="20",
-                                snapshot_interval="1", name="resnet01")
+                                criterion="BCELoss", learning_rate="0.0001", real_img_fake_label="True", num_epochs="5",
+                                noise_size="20", snapshot_interval="1", name="resnet01")
     model_path = f'./models/{unique_name}/gan_latest'
     output_path = f'./data/{unique_name}/results'
+    Evaluator.create_images(device="GPU", generator="res_net_depth1", noise_size="20", model_path=model_path, output_path=output_path)
 
-    Evaluator.create_images(device="GPU", generator="small_gan", noise_size="20", model_path=model_path, output_path=output_path)
-
-    unique_name = Trainer.train_and_get_uniquename(device="GPU", generator="res_net", discriminator="res_net",
-                                                   criterion="BCELoss", learning_rate="0.0001",
-                                                   real_img_fake_label="True", num_epochs="5", noise_size="20",
-                                                   snapshot_interval="1", name="resnet01")
+    unique_name = Trainer.train(device="GPU", generator="res_net_depth2", discriminator="res_net_depth2",
+                                criterion="BCELoss", learning_rate="0.0001", real_img_fake_label="True", num_epochs="5",
+                                noise_size="20", snapshot_interval="1", name="resnet02")
     model_path = f'./models/{unique_name}/gan_latest'
     output_path = f'./data/{unique_name}/results'
-    Evaluator.create_images(device="GPU", generator="res_net", noise_size="20", model_path=model_path,
+    Evaluator.create_images(device="GPU", generator="res_net_depth2", noise_size="20", model_path=model_path,
                             output_path=output_path)
     '''
-
-    model_path = f'./models/gan_Resnet_depth2_45E'
-    result_path = f'./models/test/results/'
-    output_path = f'./models/resnet01_2022-01-06_09-00-52_results/results'
-    Evaluator.evaluate_model(device="CPU", generator="res_net", noise_size="20", model_path=model_path,
+    model_path = 'C:/Dev/test/PraktikumNN/models/resnet02_2022-01-08_18-10-34/gan_latest'
+    result_path = f'./models/test/results2/'
+    output_path = '###'
+    Evaluator.evaluate_model(device="GPU", generator="res_net_depth2", noise_size="20", model_path=model_path,
                             output_path=output_path, result_path=result_path)
 
 if __name__ == "__main__":
-    #main()
-    inception_score_cifar10(device='CPU')
+    main()
+    #inception_score_cifar10(device='GPU', batch_size=100)
