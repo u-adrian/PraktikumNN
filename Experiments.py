dac62a3df5a16c7b971d482e079a12227f6c7e96
u-adrian <uuesb@student.kit.edu>
2022-01-10 10:35:31 +0100

    added some experiments

--- Experiments.py
+++ Experiments.py
@@ -5,34 +5,42 @@ from scores import inception_score_cifar10
 
 def main():
 
-    unique_name = Trainer.train(device="GPU", generator="small_gan", discriminator="small_gan", criterion="BCELoss",
-                                learning_rate="0.0001", real_img_fake_label="True", num_epochs="5", noise_size="20",
-                                snapshot_interval="1", name="smallGan01", batch_size=100)
-    model_path = f'./models/{unique_name}/gan_latest'
-    output_path = f'./data/{unique_name}/results'
-    Evaluator.create_images(device="GPU", generator="small_gan", noise_size="20", model_path=model_path,
-                            output_path=output_path)
-    '''
-    unique_name = Trainer.train(device="GPU", generator="res_net_depth1", discriminator="res_net_depth1",
-                                criterion="BCELoss", learning_rate="0.0001", real_img_fake_label="True", num_epochs="5",
-                                noise_size="20", snapshot_interval="1", name="resnet01")
-    model_path = f'./models/{unique_name}/gan_latest'
-    output_path = f'./data/{unique_name}/results'
-    Evaluator.create_images(device="GPU", generator="res_net_depth1", noise_size="20", model_path=model_path, output_path=output_path)
+    # unique_name = Trainer.train(device="GPU", generator="res_net_depth1", discriminator="res_net_depth1", criterion="BCELoss",
+    #                             learning_rate="0.00001", real_img_fake_label="False", num_epochs="100", noise_size="20",
+    #                             snapshot_interval="5", name="resNet01", batch_size=100)
+    # scores_path = f'C:/Dev/test/PraktikumNN/models/resNet01_2022-01-09_18-37-32/snapshots'
+    #
+    # Evaluator.evaluate_multiple_models(device="GPU", generator="res_net_depth1", noise_size="20", model_path=scores_path,
+    #                                    output_path=scores_path, batch_size=100)
 
     unique_name = Trainer.train(device="GPU", generator="res_net_depth2", discriminator="res_net_depth2",
-                                criterion="BCELoss", learning_rate="0.0001", real_img_fake_label="True", num_epochs="5",
-                                noise_size="20", snapshot_interval="1", name="resnet02")
-    model_path = f'./models/{unique_name}/gan_latest'
-    output_path = f'./data/{unique_name}/results'
-    Evaluator.create_images(device="GPU", generator="res_net_depth2", noise_size="20", model_path=model_path,
-                            output_path=output_path)
-    
-    model_path = 'C:/Dev/test/PraktikumNN/models/resnet02_2022-01-08_18-10-34/gan_latest'
-    output_path = './models/test/results2/'
-    Evaluator.evaluate_model(device="GPU", generator="res_net_depth2", noise_size="20", model_path=model_path,
-                            output_path=output_path)
-    '''
+                                criterion="BCELoss",
+                                learning_rate="0.00001", real_img_fake_label="False", num_epochs="100", noise_size="20",
+                                snapshot_interval="5", name="resNet02", batch_size=100)
+    scores_path = f'C:/Dev/test/PraktikumNN/models/{unique_name}/snapshots'
+
+    Evaluator.evaluate_multiple_models(device="GPU", generator="res_net_depth2", noise_size="20",
+                                       model_path=scores_path,
+                                       output_path=scores_path, batch_size=100)
+
+    unique_name = Trainer.train(device="GPU", generator="res_net_depth1", discriminator="res_net_depth1",
+                                criterion="BCELoss",
+                                learning_rate="0.00001", real_img_fake_label="True", num_epochs="100", noise_size="20",
+                                snapshot_interval="5", name="resNet01rifl", batch_size=100)
+    scores_path = f'C:/Dev/test/PraktikumNN/models/{unique_name}/snapshots'
+
+    Evaluator.evaluate_multiple_models(device="GPU", generator="res_net_depth1", noise_size="20",
+                                       model_path=scores_path,
+                                       output_path=scores_path, batch_size=100)
+
+    unique_name = Trainer.train(device="GPU", generator="res_net_depth2", discriminator="res_net_depth2",
+                                criterion="BCELoss",
+                                learning_rate="0.00001", real_img_fake_label="True", num_epochs="100", noise_size="20",
+                                snapshot_interval="5", name="resNet02rifl", batch_size=100)
+    scores_path = f'C:/Dev/test/PraktikumNN/models/{unique_name}/snapshots'
+
+    Evaluator.evaluate_multiple_models(device="GPU", generator="res_net_depth2", noise_size="20",
+                                       model_path=scores_path,
+                                       output_path=scores_path, batch_size=100)
 if __name__ == "__main__":
     main()
-    #inception_score_cifar10(device='GPU', batch_size=100)
