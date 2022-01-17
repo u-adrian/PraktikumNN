8bc40dc5266c48d4e2305c169d364d5ea9846647
Shadow2Castle <uoxoa@student.kit.edu>
2022-01-17 16:17:22 +0100

    Evaluator cleanup

--- Evaluator.py
+++ Evaluator.py
@@ -20,23 +20,21 @@ from tqdm import tqdm
 
 
 def evaluate_model(**kwargs):
-    #### CONSTANTS ####
+    # Constants
     NUM_CLASSES = 10
     N_IMAGE_CHANNELS = 3
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    #### Variables ####
-    # Handle Arguments
+    # Variables
     device = ArgHandler.handle_device(**kwargs)
     noise_size = ArgHandler.handle_noise_size(**kwargs)
     generator = ArgHandler.handle_generator(NUM_CLASSES, N_IMAGE_CHANNELS, **kwargs)
     model_path = ArgHandler.handle_model_path(**kwargs)
     batch_size = ArgHandler.handle_batch_size(**kwargs)
 
-    # load generator
+    # Load generator
     generator.load_state_dict(torch.load(model_path, map_location=device)['netG_state_dict'])
 
-    # initialize One Hot encoder
+    # Initialize One Hot Encoder
     one_hot_enc = OneHotEncoder()
     all_classes = torch.tensor(range(NUM_CLASSES)).reshape(-1, 1)
     one_hot_enc.fit(all_classes)
@@ -83,44 +81,41 @@ def evaluate_multiple_models(**kwargs):
     return scores_dict
 
 
-
 def create_images(**kwargs):
-    # CONSTANTS
+    # Constants
     NUM_CLASSES = 10
     N_IMAGE_CHANNELS = 3
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    #### Variables ####
-    # Handle Arguments
+    # Variables
     device = ArgHandler.handle_device(**kwargs)
     noise_size = ArgHandler.handle_noise_size(**kwargs)
     generator = ArgHandler.handle_generator(NUM_CLASSES, N_IMAGE_CHANNELS, **kwargs)
     model_path = ArgHandler.handle_model_path(**kwargs)
     output_path = ArgHandler.handle_output_path(**kwargs)
 
-    # load generator
+    # Load generator
     generator.load_state_dict(torch.load(model_path)['netG_state_dict'])
 
-    # initialize One Hot encoder
+    # Initialize One Hot encoder
     one_hot_enc = OneHotEncoder()
     all_classes = torch.tensor(range(NUM_CLASSES)).reshape(-1, 1)
     one_hot_enc.fit(all_classes)
 
-    # do it multiple times
+    # Do it multiple times
     for j in range(10):
-
-        # generate noise and stack 10 copies of that noise
+        # Generate noise and stack 10 copies of that noise
         noise = torch.randn(1, noise_size, 1, 1, device=device)
         noise = noise.repeat(10, 1, 1, 1)
 
-        # create label as one hot
+        # Create label as one hot
         labels = torch.tensor([range(10)])
         labels_one_hot = torch.tensor(one_hot_enc.transform(labels.reshape(-1, 1)).toarray(), device=device)
 
         # Generate batch (fake images + desired classes)
-        fake_images = generator(noise,labels_one_hot)
+        fake_images = generator(noise, labels_one_hot)
 
-        # as info:
+        # As info:
         # normalize = T.Normalize(mean.tolist(), std.tolist())
         # denormalize = T.Normalize((-mean / std).tolist(), (1.0 / std).tolist())
         transform = transforms.Normalize((-0.5 / 0.5, -0.5 / 0.5, -0.5 / 0.5), (1 / 0.5, 1 / 0.5, 1 / 0.5))
@@ -135,8 +130,7 @@ def create_images(**kwargs):
             im = Image.fromarray((image * 255).astype(np.uint8))
             im.save(output_path + f"/{classes[i]}_{j}.png")
 
-            ### Do we need that ?
             plt.imshow(image)
             plt.title(f'Class: {classes[i]}')
-            # plt.show()
+            plt.show()
 
