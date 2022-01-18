e7a8e33bd03aaa62870511b6026b6ceaf6104682
Shadow2Castle <uoxoa@student.kit.edu>
2022-01-18 17:45:13 +0100

    fixed xavier init

--- Nets/Utils.py
+++ Nets/Utils.py
@@ -13,7 +13,7 @@ def weights_init(m):
 def weights_init_xavier(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        nn.init.xaviernormal(m.weight.data, gain=1.0)
+        nn.init.xavier_normal_(m.weight.data, gain=1.0)
     elif classname.find('BatchNorm') != -1:
-        nn.init.xaviernormal(m.weight.data, gain=1.0)
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
